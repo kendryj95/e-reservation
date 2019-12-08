191c7a812528e88d4552e414ed11f6b215c87171
Kendry Ortiz <kendry.ortiz@corvuslab.co>
2019-12-08 14:14:34 -0500

    Commit inicial

---
+++ src/main/java/com/platzi/ereservation/negocio/repository/ReservaRepository.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.platzi.ereservation.negocio.repository;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.platzi.ereservation.modelo.Reserva;
+
+/**
+ * Interface para definir las operaciones de bdd relacionadas con reserva
+ * @author User
+ *
+ */
+public interface ReservaRepository {
+
+	@Query("SELECT r FROM Reserva r WHERE r.fechaIngresoRes = :fechaInicio AND r.fechaSalidaRes = :fechaSalida")
+	public List<Reserva> find(@Param("fechaInicio") Date fechaInicio, @Param("fechaSalida") Date fechaSalida);
+}
