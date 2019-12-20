d8bd737487e0748bfb9e01cccd444bedaa8a07c8
pedrocastillo <pedro.castillo@corvuislab.co>
2019-12-20 17:50:19 -0500

    Falto la herencia de JPARepositoy en Reserva

--- src/main/java/com/platzi/ereservation/negocio/repository/ReservaRepository.java
+++ src/main/java/com/platzi/ereservation/negocio/repository/ReservaRepository.java
@@ -6,6 +6,7 @@ package com.platzi.ereservation.negocio.repository;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -16,7 +17,7 @@ import com.platzi.ereservation.modelo.Reserva;
  * @author User
  *
  */
-public interface ReservaRepository {
+public interface ReservaRepository extends JpaRepository<Reserva, String> {
 
 	@Query("SELECT r FROM Reserva r WHERE r.fechaIngresoRes = :fechaInicio AND r.fechaSalidaRes = :fechaSalida")
 	public List<Reserva> find(@Param("fechaInicio") Date fechaInicio, @Param("fechaSalida") Date fechaSalida);
